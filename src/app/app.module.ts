3a8e7505ba02443d3ce9a2465265b31a127da6f5
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-09-28 18:45:48 +0100

    Emulator working | connection to live serve working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,51 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import {environment} from '../environments/environments.dev'
-import {initializeApp, provideFirebaseApp, getApp} from '@angular/fire/app'
-import {provideAuth, getAuth, connectAuthEmulator} from '@angular/fire/auth'
-import {provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions'
-import {provideFirestore, getFirestore, initializeFirestore, connectFirestoreEmulator, Firestore} from '@angular/fire/firestore'
+import { environment } from '../environments/environments.dev';
+import { AngularFireModule } from '@angular/fire/compat';
+import {
+  AngularFireAuthModule,
+  USE_EMULATOR as USE_AUTH_EMULATOR,
+} from '@angular/fire/compat/auth';
+import {
+  AngularFirestoreModule,
+  USE_EMULATOR as USE_FIRESTORE_EMULATOR,
+} from '@angular/fire/compat/firestore';
+import {
+  AngularFireFunctionsModule,
+  USE_EMULATOR as USE_FUNCTIONS_EMULATOR,
+} from '@angular/fire/compat/functions';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => {
-      let firestore: Firestore;
-      if(environment.useEmulators){
-        // Long polling required for cypress ??
-        firestore = initializeFirestore(getApp(), {
-          experimentalForceLongPolling: true
-        });
-        connectFirestoreEmulator(firestore, 'localhost', 8080);
-      } else {
-        firestore = getFirestore()
-      }
-      return firestore;
-    }),
-    provideAuth(() => {
-      const auth = getAuth();
-      if(environment.useEmulators){
-        connectAuthEmulator(auth, 'http://localhost:9099', {
-          disableWarnings: true
-        })
-      }
-      return auth;
-    }),
-    provideFunctions(() => {
-      const functions = getFunctions();
-      if(environment.useEmulators){
-        connectFunctionsEmulator(functions, 'localhost', 5001)
-      }
-      return functions
-    })
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireFunctionsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: USE_AUTH_EMULATOR,
+      useValue: environment.useEmulators
+        ? ['http://localhost', 9099]
+        : undefined,
+    },
+    {
+      provide: USE_FIRESTORE_EMULATOR,
+      useValue: environment.useEmulators
+        ? ['localhost', 8080]
+        : undefined,
+    },
+    {
+      provide: USE_FUNCTIONS_EMULATOR,
+      useValue: environment.useEmulators
+        ? ['localhost', 5001]
+        : undefined,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
