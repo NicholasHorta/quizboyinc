c82499e868f079e4ccce66470f5ba565ee4d8c3e
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-09-29 22:27:00 +0100

    Shared module with multi components and index/modules exports added

--- client/Ng-Specific.js
+++ client/Ng-Specific.js
@@ -31,5 +31,75 @@
 //> In our root, keep it as cleamn as possible
 //> If a component/service has unique dependencies, this is a sign to remove this component/service into it's own module and import all dependencies into it
 
+//. Dividing an App into Modules & Components
+//: Can have their own modules
+//  > Module
+//:   > components
+//:   > services
+//:   > directives
+//>   > pipes
+//>   > guards
+//>   > resolvers
+//>   > models
+//>   > store
+//:   > pages
+//:   > shared
 
+/// The above will give an application greater capability for extension
+
+//. When is it time to add seperate components
+//> At the first opportunity to split the logic of the parent component
+//. Make a seperate component if an elements has:
+//> Its own logic of interaction with the user
+//> A complicated layout / animation
+//> Any methods which can distract from understanding the logic of the parent component
+//. Method
+//> Take a look at the layout
+//@   Visually separate elements can be converted into separate components
+//> Do separate components have their own dependencies
+//@   If unique set of additional modules is needed which are not used by parent or neighbor components
+//@   eg: if one of the areas is a form that requires ReactiveFormsModule
+//> Is it possible to divide components into sub-components
+//@   If a component can be divided into sub-components, you need to add a module to it
+//- EG: Header component which has:
+//-   - Sign up/Log in
+//-   - Menu items
+/// Can be transformed to the following structure
+//- Header.component.ts
+//-   [] components
+//-     -> [] Menu Component
+//-     -> [] Links Component
+//-     -> index.ts
+
+//: In a module of a parent component, you should declare those components which are DIRECTLY USED by this module
+//: Ref ID001.png
+
+//. Shared
+//> Shared = Fully re-usable components of the application
+//> Must NOT be an all in one module - Smae approach for others should be taken
+//: Even if a module has 1000 components, if we specify ONE it will only load in one.
+//: It's best practice for EACH component in shared module to have it's OWN module
+//> This way, they can be a part of the Shared Module and at the same time may be seperately imported into the PageModule
+
+
+//. Advantages of Single Responsibility Components
+//: Eg: Button Component - Seems redundant, But..
+//> As the applicatoin grows and get's more complicated, these small components become reusable without affecting the focus of the main components
+//: let's say we have 4 pages, each with a button.
+//: Each button has it's own string and event
+//> So we just made a library out of a component and ultimately a module delivers & unpacks it
+//- Button.component.ts
+//-   @Input label: string;
+//-   @Output() click: EventEmitter;
+//-   @Output() mouseOver: EventEmitter;
+//-   @Output() mouseOut: EventEmitter;
+//> The more distant the component is from the endpoint of use, the more abstract it should be
+//> This is why libraries use tons of callbacks and custom parameters
+//> They don't have a clue ablut the final implementation!
+//: It's always easier to start with feature components, If you aren't sure a new component will be re-used - Don't Drag It Into Shared
+
+//$ Avoid creating business logic components in global shared!
+//> If a page 1, 2, & 3 use the same component --> ID002.png
+//> We Don't need it in our global shared module even if several use it within the same module
+//> We simply make a shared section within this component --> ID003.png
 
