4f24cac07b11fb9b7ae7474a635bee6db0dca342
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-10-09 18:32:14 +0100

    Fonts imported

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './shared/logo/logo.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, LogoComponent],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ]
 })
 export class CoreModule {}
