3473f257c63a583b1d3092c56702bc8448d870e9
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-11-09 23:56:14 +0000

    feature: configure firebase service

--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  constructor(private ngFirestore: AngularFirestore) {}
+
+  get db(): AngularFirestore{
+    return this.ngFirestore;
+  }
+
+  get createId(): string {
+    return this.ngFirestore.createId();
+  }
 }
--- src/app/services/quiz/quiz.service.ts
+++ src/app/services/quiz/quiz.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { FirebaseService } from '../firebase/firebase.service';
+import { Observable } from 'rxjs';
 
+
+interface Test {
+  give: {
+    one: number
+  },
+  totalArr: string[]
+
+}
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
+  constructor(private firebaseSVC: FirebaseService) {}
 
-  constructor() { }
+  getTestCollection(): Observable<any> {
+    return this.firebaseSVC.db.doc<Test>('test/Eb7J6716fJ19Gf8RQJvW').valueChanges()
+  }
 }
