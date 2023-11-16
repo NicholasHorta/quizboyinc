3d56a84825e1e88f8ffc05ce2c75c11863a6bc00
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-11-16 22:54:28 +0000

    feat: quiz question scaffolding

--- src/app/components/quiz-shows/quiz-question/quiz-question.component.ts
+++ src/app/components/quiz-shows/quiz-question/quiz-question.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuizService } from '@app/services/quiz/quiz.service';
 
 @Component({
   selector: 'bs-quiz-question',
   templateUrl: './quiz-question.component.html',
   styleUrls: ['./quiz-question.component.scss']
 })
-export class QuizQuestionComponent {
+export class QuizQuestionComponent implements OnInit {
+
+  confirmQuizInit: boolean = false;
+
+  constructor(private quizSVC: QuizService, private activeRoute: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    const showId = this.activeRoute?.snapshot?.paramMap?.get('show');
+    this.quizSVC.getSeasonQuizData(showId ?? '').subscribe(data => console.log(data));
+  }
+
+  initiateQuiz(): void {
+    this.confirmQuizInit = true;
+  }
 
 }
--- src/app/services/quiz/quiz.service.ts
+++ src/app/services/quiz/quiz.service.ts
@@ -3,7 +3,6 @@ import { FirebaseService } from '../firebase/firebase.service';
 import { Observable, of } from 'rxjs';
 import { ShowWithId } from '@app/models/quiz.interface';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,10 +10,18 @@ export class QuizService {
   constructor(private firebaseSVC: FirebaseService) {}
 
   get shows(): Observable<any> {
-    return this.firebaseSVC.db.collection('shows').valueChanges({idField: 'id'}) as unknown as Observable<ShowWithId>;
+    return this.firebaseSVC.db
+      .collection('shows')
+      .valueChanges({ idField: 'id' }) as unknown as Observable<ShowWithId>;
   }
 
   getSeasons(show: string): Observable<any> {
     return this.firebaseSVC.db.collection('shows').doc(show).valueChanges();
+  }
+
+  getSeasonQuizData(showId: string) {
+    return this.firebaseSVC.db
+      .collectionGroup('seasons', ref => ref.where('showId', '==', showId))
+      .valueChanges({ idField: 'id' });
   }
 }
