3151bce39a245a217dc491865bcc4867b2b98adc
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-12-10 19:32:18 +0000

    JEST CONFIGURED - Initial test with Profile component

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProfileComponent } from './profile.component';
+import { ProfileService } from '@app/services/profile/profile.service';
+import { of } from 'rxjs';
+import { UserMock } from '@app/shared/mocks/data.mocks';
+import { By } from '@angular/platform-browser';
+
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [ ProfileComponent ],
+      providers: [
+        {provide: ProfileService, useValue: {currentUser$: of(UserMock)}}
+      ]
     })
     .compileComponents();
 
@@ -17,7 +23,16 @@ describe('ProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Profile component', () => {
     expect(component).toBeTruthy();
   });
+describe('ProfileComponent', () => {
+  // const profileWrapper = ;
+  it('should reflect correct username', () => {
+    const username = fixture.debugElement.query(By.css('[data-testid="profile-wrapper"]')).query(By.css('h1')).nativeElement.textContent;
+    expect(username).toEqual('TestUser69')
+  })
+})
+
+
 });
