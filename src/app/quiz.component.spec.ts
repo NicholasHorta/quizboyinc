7d062a5eb752f8a6583672462aab78ddd29eee72
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-12-10 17:33:56 +0000

    test file fixes

--- src/app/quiz.component.spec.ts
+++ src/app/quiz.component.spec.ts
@@ -1,33 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './quiz.component';
+import { QuizComponent } from './quiz.component';
 
-describe('AppComponent', () => {
+describe('QuizComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        QuizComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(QuizComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'quiz'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(QuizComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('quiz');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(QuizComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('quiz app is running!');
