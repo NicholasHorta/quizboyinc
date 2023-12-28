4e351246925f0c46a85fbcf209c1038659b33e03
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2023-12-28 22:45:55 +0000

    feat: password reset

---
+++ src/app/core/components/user-portal/reset-password/reset-password.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonComponent } from '@app/shared/components/button/button.component';
+import { UserService } from '@app/services/auth/user.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'bs-reset-password',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, ButtonComponent],
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.scss']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  constructor(private userSVC: UserService) { }
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+  });
+  error$: Observable<string>;
+
+  ngOnInit(): void {
+    this.error$ = this.userSVC.error$;
+  }
+
+  requestResetPassword() {
+    this.userSVC.resetPassword(this.form.value.email)
+  }
+
+}
