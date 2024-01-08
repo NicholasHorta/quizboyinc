4b4912c6b0e85797f78b535abd5cbeff44122fe1
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2024-01-08 10:44:53 +0000

    Custom Colors updated - Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,19 @@ module.exports = {
       colors: {
         'qz-pink-alt': '#e60073',
         'qz-pink': '#ff66b2',
+        'qz-pink-op': '#ffccf2',
         'qz-red-alt': '#ff0033',
         'qz-red': '#ff6666',
+        'qz-red-op': '#ffcccc',
         'qz-green': '#66ff66',
+        'qz-green-op': '#ccffcc',
         'qz-green-alt': '#00cc00',
         'qz-blue-alt': '#0099ff',
         'qz-blue': '#66ccff',
+        'qz-blue-op': '#cce5ff',
         'qz-yellow-alt': '#ffcc00',
-        'qz-yellow': '#ffdb4d'
+        'qz-yellow': '#ffdb4d',
+        'qz-yellow-op': '#ffe0b3'
       }
     }
   },
