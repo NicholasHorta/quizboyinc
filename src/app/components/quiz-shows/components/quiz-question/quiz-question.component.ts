87d292e2360e43ea70896f1caa368ba862075727
Nicholas Da Silva Horta <nickdeveloper.js@gmail.com>
2024-01-20 21:31:17 +0000

    fix: answer selection style application fixed

--- src/app/components/quiz-shows/components/quiz-question/quiz-question.component.ts
+++ src/app/components/quiz-shows/components/quiz-question/quiz-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, Output, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, Output, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
 import { AnswerEmit, Questions, Timer } from '@app/models/quiz.models';
 import { QuizButton } from '@app/models/shared/global.models';
 
@@ -8,15 +8,25 @@ import { QuizButton } from '@app/models/shared/global.models';
   templateUrl: './quiz-question.component.html',
   styleUrls: ['./quiz-question.component.scss']
 })
-export class QuizQuestionComponent {
+export class QuizQuestionComponent implements OnChanges {
   @Input() data: Questions[];
   @Input() timer: Timer;
   @Input() quizBtnState: QuizButton;
   @Input() questionIndex: number;
   @Output() emitSetSelectedAnswer: EventEmitter<AnswerEmit> = new EventEmitter<AnswerEmit>();
+  @Output() emitResetAnswers: EventEmitter<AnswerEmit> = new EventEmitter<AnswerEmit>();
   @ViewChildren('selectedAnswer') selectedAnswer: QueryList<ElementRef<HTMLButtonElement>>;
 
   onSetSelectedAnswer(answer: string): void {
     this.emitSetSelectedAnswer.emit({ answer, btnRef: this.selectedAnswer });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes && changes['questionIndex']){
+      this.emitResetAnswers.emit({ answer: '', btnRef: this.selectedAnswer });
+      // this.selectedAnswer.forEach((btn: ElementRef<HTMLButtonElement>) => {
+      //   btn.nativeElement.classList.remove('selected');
+      // });
+    }
   }
 }
